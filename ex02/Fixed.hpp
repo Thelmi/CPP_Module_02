59a9f7191aaddb7504eae9dea3c662818d8ceb04
Taha Helmy <thelmy@lab1r1s12.42abudhabi.ae>
2024-11-16 08:57:22 +0400

    ex02 and excercses

---
+++ ex02/Fixed.hpp
@@ -0,0 +1,34 @@
+#ifndef FIXED_HPP
+#define FIXED_HPP
+#include <cmath>
+#include <iostream>
+
+class Fixed
+{
+	private:
+		int fixedNum;
+		static const int bits = 8;
+	public:
+		Fixed();
+		Fixed(const Fixed &numObj);
+		Fixed(const int intNum);
+		Fixed(const float floatNUM);
+		~Fixed();
+		Fixed &operator=(const Fixed &numObj);
+		void setRawBits(int const raw);
+		int getRawBits() const;
+		float toFloat() const;
+		int toInt() const;
+		bool operator>(const Fixed &numObj);
+		bool operator<(const Fixed &numObj);
+		bool operator>=(const Fixed &numObj);
+		bool operator<=(const Fixed &numObj);
+		bool operator!=(const Fixed &numObj);
+		int operator+(const Fixed &numObj);
+		int operator-(const Fixed &numObj);
+		int operator*(const Fixed &numObj);
+		int operator/(const Fixed &numObj);
+};
+
+std::ostream& operator<<(std::ostream& os, const Fixed &obj);
+#endif
